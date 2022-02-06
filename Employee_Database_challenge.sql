21c68749e082336233a8c2649ad6efb7e1f9d5f6
SarahMason2015 <95657455+SarahMason2015@users.noreply.github.com>
2022-02-06 15:37:49 -0600

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -18,3 +18,19 @@ GROUP BY ut.title
 ORDER BY COUNT DESC
 
 
+---
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+e.first_name, 
+e.last_name, 
+e.birth_date, 
+de.from_date, 
+de.to_date,
+t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (de.emp_no = e.emp_no)
+INNER JOIN titles as T
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
